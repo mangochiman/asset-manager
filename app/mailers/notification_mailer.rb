4611819f67616b57e1d8c542df645e09f51170e0
Ernest Matola <mangochiman@gmail.com>
2023-09-19 09:13:52 +0200

    Emails

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,16 @@
+class NotificationMailer < ApplicationMailer
+
+  settings = YAML.load_file(Rails.root.to_s + '/config/settings.yml')['settings']
+  default from: "Asset Manager <#{settings['email']}>"
+
+  def reset_password(person, new_password)
+    @name = person.first_name
+    @new_password = new_password
+    mail(
+        to: user.email,
+        subject: 'PASSWORD RESET'
+    )
+  end
+
+
+end
